d76bc9b6cc4b846f409a1ae463f2d47abc9c61d4
padouch <padouch@centrum.cz>
2021-01-20 01:15:52 +0100

    add templating

--- app/answer_add/views.py
+++ app/answer_add/views.py
@@ -12,7 +12,6 @@ def answeradd():
 
     if request.method == 'GET':
         ar = Answer.query.filter_by(q_id=qr_id)
-        #        question = db.engine.execute ("select q_txt from question where id = 1")
         qr = Question.query.filter_by(id=qr_id)
         return render_template('answer_add/answeradd.html', answers=ar, quest_id=qr_id, que=qr)
 
@@ -24,3 +23,34 @@ def answeradd():
     #   question = db.engine.execute("select q_txt from question where id = 1")
     qr = Question.query.filter_by(id=q_id)
     return render_template('answer_add/answeradd.html', answers=ar, quest_id=q_id, que=qr)
+
+
+@mod.route('/answertmpl', methods=['POST'])
+def answertmpl():
+    qr_id = request.form.get('q_id')
+    a_tmpl = request.form.get('tmp_txt')
+    if a_tmpl == "scale1-5-aggree":
+        print("scale1-5-aggree")
+        a_add = create_answer("Strongly Disagree", qr_id)
+        a_add = create_answer("Disagree", qr_id)
+        a_add = create_answer("Undecided", qr_id)
+        a_add = create_answer("Agree", qr_id)
+        a_add = create_answer("Strongly Agree", qr_id)
+    elif a_tmpl == "scale1-5-improve":
+        print("scale1-5-improve")
+        a_add = create_answer("Needs Improvement", qr_id)
+        a_add = create_answer("Advanced", qr_id)
+        a_add = create_answer("Leader", qr_id)
+        a_add = create_answer("Master", qr_id)
+        a_add = create_answer("Expert and excelling", qr_id)
+    elif a_tmpl == "boolean-tmpl":
+        print("boolean-tmpl")
+        a_add = create_answer("Yes", qr_id)
+        a_add = create_answer("No", qr_id)
+    else:
+        print("wrong param")
+
+    ar = Answer.query.filter_by(q_id=qr_id)
+    #        question = db.engine.execute ("select q_txt from question where id = 1")
+    qr = Question.query.filter_by(id=qr_id)
+    return render_template('answer_add/answeradd.html', answers=ar, quest_id=qr_id, que=qr)
